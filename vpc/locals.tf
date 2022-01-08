55a3666f2dc9dd9b17cd424fa89edc421dd7fccc
saisuda9 <saikumarsuda@gmail.com>
2022-01-08 12:54:33 +0530

    adding new lines

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  DEFAULT_VPC_CIDR =  split(",", var.DEFAULT_VPC_CIDR)
-  ALL_VPC_CIDR = concat(local.DEFAULT_VPC_CIDR, var.VPC_CIDR_ADDON)
+  VPC_CIDR_MAIN=  split(",", var.VPC_CIDR_MAIN)
+  ALL_VPC_CIDR = concat(local.VPC_CIDR_MAIN, var.VPC_CIDR_ADDON)
 }
 
 output "ALL_VPC_CIDR" {
