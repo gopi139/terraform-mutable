7d2b4bc11dadf10d60d125d9c1f96dbacdd3dd6d
saisuda9 <saikumarsuda@gmail.com>
2022-01-08 13:15:05 +0530

    adding new lines

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -12,8 +12,8 @@ resource "aws_route_table" "route" {
   }
 }
 resource "aws_route" "route-from-default-vpc" {
-  count                     = length(data.aws_route_tables.default-vpc-route.ids)
-  route_table_id            = element(data.aws_route_tables.default-vpc-route.ids,count.index)
+  count                     = length(tolist(data.aws_route_tables.default-vpc-route.ids))
+  route_table_id            = element(tolist(data.aws_route_tables.default-vpc-route.ids),count.index)
   destination_cidr_block    = [for s in local.ALL_VPC_CIDR : s]
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
